5c7a0309766098fc9bac9d4f89dd00f158eb393d
dudetu <dudetu34@gmail.com>
2023-07-03 18:17:04 +0200

    add+3

---
+++ src/main/java/com/example/app_bank1/other_paymens/categories/entite/PaymentRequest.java
@@ -0,0 +1,53 @@
+package com.example.app_bank1.other_paymens.categories.entite;
+
+import com.example.app_bank1.account.PaymentApiResponse;
+import com.example.app_bank1.other_paymens.categories.BankAccountPayment;
+import com.example.app_bank1.repository.AccountTransferRepository;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "payment_request")
+public class PaymentRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "amoun")
+    private BigDecimal amount;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "bank_accoun")
+    private String bankAccount;
+
+
+    public void makePayment(BankAccountPayment bankAccountPayment) {
+        AccountTransferRepository bankAccountPaymentRepository = null;
+        bankAccountPaymentRepository.save(bankAccountPayment);
+
+        String apiUrl = "https://api.payment-system.com/payments";
+
+        // Инициализируйте и заполните paymentRequest перед использованием
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setAmount(bankAccountPayment.getAmount());
+        paymentRequest.setUserId((Long) bankAccountPayment.getUserId());
+        paymentRequest.setBankAccount((String) bankAccountPayment.getBankAccount());
+        paymentRequest.setId(bankAccountPayment.getId());
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            PaymentApiResponse response = restTemplate.postForObject(apiUrl, paymentRequest, PaymentApiResponse.class);
+            // Обработайте ответ от API платежной системы, если необходимо
+        } catch (Exception e) {
+            // Обработайте исключение при вызове API платежной системы
+        }
+    }
+}
---
+++ src/main/java/com/example/app_bank1/service/PaymentApiService.java
@@ -0,0 +1,7 @@
+package com.example.app_bank1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentApiService {
+}
